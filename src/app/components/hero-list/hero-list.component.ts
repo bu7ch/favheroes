de86193ee18a71444ad720435add465d526d7001
Rodolphe <dutchr@live.fr>
2021-02-15 11:46:28 +0100

    modification du composant hero-list & ajout du model hero

--- src/app/components/hero-list/hero-list.component.ts
+++ src/app/components/hero-list/hero-list.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from 'src/app/hero.model';
 @Component({
   selector: 'app-hero-list',
   templateUrl: './hero-list.component.html',
   styles: [],
 })
 export class HeroListComponent implements OnInit {
-  heroes: String[];
+  heroes: Hero[]
   constructor() {}
 
   ngOnInit(): void {
     this.heroes = [
-      "Batou",
-      "Spidou",
-      "Dr.Octopus"
+      {id: 1, name: 'Batou'},
+      {id: 2, name: 'Spidou'},
+      {id: 3, name: 'Dr.Octopus'},
+      
     ];
   }
 }
