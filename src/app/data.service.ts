bca64d3a41c3d50861855fd85074448efc7f3f2e
Rodolphe <dutchr@live.fr>
2021-02-15 13:08:25 +0100

    update data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,14 +6,17 @@ import { Hero } from './hero.model';
 })
 export class DataService {
   constructor() {}
-  heroes: Hero[] = [
-    { id: 1, name: 'Superman' },
-    { id: 2, name: 'Batman' },
-    { id: 3, name: 'Spiderman' },
-    { id: 4, name: 'Ironman' },
-    { id: 5, name: 'Thanos' },
-    { id: 6, name: 'Octopus' },
-    { id: 7, name: 'Hulk' },
-    { id: 8, name: 'Thor' },
-  ];
+
+  getHeroes(): Hero[] {
+    return [
+      { id: 1, name: 'Superman' },
+      { id: 2, name: 'Batman' },
+      { id: 3, name: 'Spiderman' },
+      { id: 4, name: 'Ironman' },
+      { id: 5, name: 'Thanos' },
+      { id: 6, name: 'Octopus' },
+      { id: 7, name: 'Hulk' },
+      { id: 8, name: 'Thor' },
+    ];
+  }
 }
