37c105dfed2d96d15f18e9d24da7c2ea6004e636
Rodolphe <dutchr@live.fr>
2021-02-15 11:58:31 +0100

    add data by Angular service

---
+++ src/app/data.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  constructor() {}
+  heroes: Hero[] = [
+    { id: 1, name: 'Superman' },
+    { id: 2, name: 'Batman' },
+    { id: 3, name: 'Spiderman' },
+    { id: 4, name: 'Ironman' },
+    { id: 5, name: 'Thanos' },
+    { id: 6, name: 'Octopus' },
+    { id: 7, name: 'Hulk' },
+    { id: 8, name: 'Thor' },
+  ];
+}
