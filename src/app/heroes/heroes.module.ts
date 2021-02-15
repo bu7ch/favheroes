872c90b1609c4be39a2e60cdafe84660cf79e92c
Rodolphe <dutchr@live.fr>
2021-02-15 14:09:00 +0100

    Display Data from Service into HeroListComponent

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroListComponent } from './components/hero-list/hero-list.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 
 
 @NgModule({
-  declarations: [HeroListComponent],
+  declarations: [HeroListComponent, HeroDetailComponent],
   imports: [
     CommonModule
   ],
